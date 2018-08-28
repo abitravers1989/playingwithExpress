2c0de93f9bf1195f792777d4d44723bb0b101de3
abitravers1989 <atravers4007@gmail.com>
2018-08-28 16:47:58 +0100

    playing with express

--- index.js
+++ index.js
@@ -5,22 +5,31 @@ const fs = require('fs');
 
 const app = express();
 
-app.set('title', 'my site')
-console.log(app.get('title'))
-
 console.log(app.get('/'))
 
 app.get('/', function (req, response) {
-    console.log(response)
+    console.log(req)
     response.send('hello world')
 })
 
+app.get('/hi', (req, res) => {
+    readingFile(res)
+})
 
-app.set('/hi', 'my site')
-app.get('/hi', function (req, res) {
+const readingFile = function (res) {
     fs.readFile('hello.md', (error, data) => {
         if (error) throw error;
-        response.send(data.toString());
+        res.send(data.toString());
     })
-})
+}
+
+// app.get('/hi', function (req, res) {
+//     fs.readFile('hello.md', (error, data) => {
+//         if (error) throw error;
+//         res.send(data.toString());
+//     })
+// })
+
+
+app.listen(3000)
 
