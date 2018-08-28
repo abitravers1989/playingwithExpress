d4f5b2b6b0d49a3752ae159719c7b34f37259599
abitravers1989 <atravers4007@gmail.com>
2018-08-28 17:51:52 +0100

    trying to understand next()

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app = express();
 console.log(app.get('/'))
 
 app.get('/', function (req, response) {
-    console.log(req)
+    //console.log(req.cookies)
     response.send('hello world')
+    //response.writeHead(statusCode: status.valueOf)
 })
 
 app.get('/hi', (req, res) => {
@@ -23,13 +24,41 @@ const readingFile = function (res) {
     })
 }
 
-// app.get('/hi', function (req, res) {
-//     fs.readFile('hello.md', (error, data) => {
-//         if (error) throw error;
-//         res.send(data.toString());
-//     })
-// })
+app.get('/errorHandling', (req, res, next) => {
+    fs.readFile('non-existant-file', (error, data) => {
+        if (error) {
+            //pass this error to express
+            next(error)
+        }
+        else {
+            res.send(data)
+        }
+    })
+})
+
+app.get('/new', (req, res, next) => {
+    //console.log(next())
+    res.send("working?")
+    next()
+    console.log(next())
+})
 
 
-app.listen(3000)
+const myLogger = (req, res, next) => {
+    res.send(console.log('Logged'))
+    next();
+}
+
+app.use(myLogger)
+
+app.get('/logger', (req, res) => {
+    myLogger()
+})
+
+
+
+const printPort = (portNumber) => { console.log(`listening on port ${portNumber}`) }
+
+const port = 3000
+app.listen(port, printPort(port))
 
