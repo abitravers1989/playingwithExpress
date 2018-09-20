2576ebb13e4c094cfd6b973c4900edbf1f535689
abitravers1989 <atravers4007@gmail.com>
2018-09-20 18:45:46 +0100

    Starting to make test pass

---
+++ middleware/postValidation.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+    return (post) => {
+        console.log(post);
+    }
+}
+
+//need to work out how module.exports works 
--- middleware/postValidation.test.js
+++ middleware/postValidation.test.js
@@ -1,9 +1,15 @@
 const { expect } = require('chai');
+const { v1 } = require('uuid');
+const { postValidation } = require('./postValidation');
 
 describe('PostValidation', () => {
     describe('when it recieves a post with an invalid title', () => {
+        const author = 'Abi';
+        const content = 'First Post';
+        const invalidPost = { title: null, content: content, date: Date.now(), id: v1(), author: author }
+        const expectedErrorMessage = 'A valid post title must be provided.'
         it('throws an Error', () => {
-            expect(true).to.be.true;
+            expect(postValidation(invalidPost)).to.throw(TypeError, expectedErrorMessage);
         })
     })
 })
