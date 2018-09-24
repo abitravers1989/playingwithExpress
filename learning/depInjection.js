d04096eb5c8c7243efb38221c790369a6746a54d
abitravers1989 <atravers4007@gmail.com>
2018-09-24 12:49:13 +0100

    Working with promises

--- learning/depInjection.js
+++ learning/depInjection.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+
+app.use(session({
+    store: require('connect-session-knex')
+}))
--- learning/promise.js
+++ learning/promise.js
@@ -1,13 +1,42 @@
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject('didnt work')
-    }, 1000);
-});
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject(Error('didnt work'));
+//     }, 1000);
+// });
 
-p
-    .then(data => {
-        console.log(data)
+// p
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+const posts = [
+    { title: 'i love js', author: 'wes bos', id: 1 },
+    { title: 'sankes', author: 'snake master', id: 2 },
+    { title: 'pencilcases', author: 'cat friend', id: 3 },
+];
+
+const authors = [
+    { name: 'wes bos', bio: 'sommee' },
+    { name: 'snake master', bio: 'sommee2' },
+    { name: 'cat friend', bio: 'sommee3' },
+]
+
+// function getPostById(id) {
+
+// }
+
+function getPostById(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(posts.find(element => element.id === id))
+        }, 200)
     })
-    .catch(err => {
-        console.log(err);
-    })
+}
+
+getPostById(2)
+    .then(data => { console.log(data) });
+
+console.log("timing")
